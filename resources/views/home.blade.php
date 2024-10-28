4050a7a5e6e6a4b9cb2e70362fc3f6ca95219b14
LIinz27 <dasril.hasdar27@gmail.com>
2024-10-28 16:13:21 +0800

    Add Sampul

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 @include('header') <!-- Menyertakan header -->
 
-<div class="container mt-4">
-    <h1>Welcome to Dealer Hyundai </h1>
-    
+<div class="container mt-3">
+    <div>
+        <img src="{{ asset('images/SAMPUL-WEB-1.png') }}" alt="Gambar Sampul" class="img-fluid rounded w-100" style="max-height: 400px; object-fit: cover;" />
+    </div>
 </div>
