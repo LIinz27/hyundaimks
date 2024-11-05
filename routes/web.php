5659b10a71f7c497e8b1e6bd8800e260ec116b60
LIinz27 <dasril.hasdar27@gmail.com>
2024-11-05 09:34:35 +0800

    add pricelist page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage/home');
 });
+
+Route::get('/pricelist', [App\Http\Controllers\PricelistController::class, 'index']);
