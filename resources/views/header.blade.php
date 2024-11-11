d82eff14e7cd1e5d584ca57919af35067cbff828
LIinz27 <dasril.hasdar27@gmail.com>
2024-11-11 15:46:59 +0800

    Header Hidden Animation

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 
-    <header class="navbar navbar-expand-lg navbar-light py-3">
+    <header class="navbar navbar-expand-lg navbar-light py-3" id="header">
         <div class="container d-flex justify-space-between">
             <a href="{{ url('/') }}"><img src="{{ asset('images\LOGO-HYUNDAI.png') }}" alt="Gambar logo" class="img-fluid" style="max-height: 30px; object-fit: cover;" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
